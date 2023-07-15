0ab6c7c67535bd5a01bd13c0dffa643059a48288
redzx69 <115964926+redzx69@users.noreply.github.com>
2023-07-16 00:11:00 +0800

    added getDayOfWeek

--- src/HospitalApp.java
+++ src/HospitalApp.java
@@ -1,6 +1,7 @@
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.time.DayOfWeek;
 import java.time.LocalDate;
 import java.util.Random;
 import java.util.Scanner;
@@ -56,7 +57,7 @@ public class HospitalApp {
 
     public static void displayMenu() {
         System.out.println("\n===== Hospital Management System =====");
-        System.out.println("Date: " + currentDate);
+        System.out.println("Date: " + currentDate + "\tDay: " + currentDate.getDayOfWeek());
         System.out.println();
         System.out.println("1. Add Doctor");
         System.out.println("2. Add Patient");
@@ -124,6 +125,7 @@ public class HospitalApp {
     // choice no.3: Add Ward Room
     public static void addWardRoom() {
         clearScreen();
+
         for (int i = 0; i < MAX_WARDROOMS; i++) {
             Random random = new Random(50); // random number for ward room
             System.out.println("<<<<<<<<<< ASSIGN PATIENT TO WARD ROOM >>>>>>>>>>");
