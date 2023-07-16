e9133826b6dc5bba1d765d9a2771331543cf105a
redzx69 <115964926+redzx69@users.noreply.github.com>
2023-07-16 13:18:16 +0800

    added total price in console and txt

--- src/Patient.java
+++ src/Patient.java
@@ -1,5 +1,3 @@
-import java.util.Scanner;
-
 public class Patient extends Person {
     private String diagnosis;
     private String admissionDate;
@@ -31,19 +29,18 @@ public class Patient extends Person {
 
 
     //processor
-    public double priceDeduction() {
-        Scanner scan = new Scanner(System.in);
-        double coverage = 0.0, total = 0.0;
-        if(insurance == true) {
-            System.out.print("Coverage of that patient's insurance [in percentage]: ");
-            coverage = scan.nextDouble();
-        }
-        else{
-            coverage = 100;
-        }
-        total = coverage/100;
-        return total;
-    }
+    // public double priceDeduction() {
+    //     Scanner scan = new Scanner(System.in);
+    //     double coverage = 0.0, total = 0.0;
+    //     if(insurance == true) {
+
+    //     }
+    //     else{
+    //         coverage = 100;
+    //     }
+    //     total = coverage/100;
+    //     return total;
+    // }
 
     //printer
     public String toString(){
